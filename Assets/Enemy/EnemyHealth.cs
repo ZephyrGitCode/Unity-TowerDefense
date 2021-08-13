b487a3a8928a185ea736e9a5cb8fb061fba51744
ZephyrGitCode <zephyr.dobson@outlook.com>
2021-08-14 08:47:44 +0930

    git

---
+++ Assets/Enemy/EnemyHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHitPoints = 5;
+    [Tooltip("Adds additional max health to enemy.")]
+    [SerializeField] int diffucltiyRamp = 1;
+    int currentHitPoints = 0;
+
+    Enemy enemy;
+
+    private void Start() {
+        enemy = GetComponent<Enemy>();
+    }
+
+    void OnEnable()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+    
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+    void ProcessHit()
+    {
+        currentHitPoints--;
+
+        if(currentHitPoints <= 0)
+        {
+            gameObject.SetActive(false);
+            maxHitPoints += diffucltiyRamp;
+            enemy.RewardGold();
+        }
+    }
+}
