dcb83d33dc934cd9f83545f305c2c771b1bec95e
ZephyrGitCode <zephyr.dobson@outlook.com>
2021-08-21 10:49:39 +0930

    Before removing path

--- Assets/Pathfinding/GridManager.cs
+++ Assets/Pathfinding/GridManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] Vector2Int gridSize;
+    [Tooltip("World grid size should match unity editor snap settings")]
+    [SerializeField] int unityGridSize = 10;
+    public int UnityGridSize { get { return unityGridSize; } }
 
     Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
     public Dictionary<Vector2Int, Node> Grid { get { return grid; } }
@@ -19,6 +22,31 @@ public class GridManager : MonoBehaviour
             return grid[coordinates];
         }
         return null;
+    }
+
+    public void BlockNode(Vector2Int coordinates)
+    {
+        if(Grid.ContainsKey(coordinates))
+        {
+            grid[coordinates].isWalkable=false;
+        }
+    }
+
+    public Vector2Int GetCoordinatesFromPosition(Vector3 position)
+    {
+        Vector2Int coordinates = new Vector2Int();
+        coordinates.x = Mathf.RoundToInt(position.x / unityGridSize);
+        coordinates.y = Mathf.RoundToInt(position.z / unityGridSize);
+
+        return coordinates;
+    }
+
+    public Vector3 GetPositionFromCoordinates(Vector2Int coordinates)
+    {
+        Vector3 position = new Vector3();
+        position.x = Mathf.RoundToInt(coordinates.x * unityGridSize);
+        position.z = Mathf.RoundToInt(coordinates.y * unityGridSize);
+        return position;
     }
 
     void CreateGrid()
